baa52866db5e77bc486b93cdda67acc14a0245ea
Magno Urbano <magnourbano@katkay.com>
2020-12-17 03:43:23 +0000

    Initial Commit

---
+++ Teste/TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteApp.swift
+//  Teste
+//
+//  Created by Mars on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
